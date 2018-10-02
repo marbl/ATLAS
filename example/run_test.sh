9dc3d4bb9f7f1ddd7e98f60975fb9561990c90b6
shahnidhi <nidhirshah1992@gmail.com>
2018-10-02 18:02:20 -0400

    Moving away from max_target_seqs parameter

--- example/run_test.sh
+++ example/run_test.sh
@@ -2,7 +2,7 @@
 makeblastdb -in database.fasta -out database.fasta -dbtype nucl
 
 # Doing BLAST search
-blastn -query query.fasta  -db database.fasta -max_target_seqs 100 -outfmt " 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qseq sseq qlen " -task megablast -out blast.out -num_threads 4
+blastn -query query.fasta  -db database.fasta  -outfmt " 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qseq sseq qlen " -task megablast -out blast.out -num_threads 4
 
 # Detecting outlier BLAST hits for query sequences
 python ../score_blast.py -q query.fasta -b blast.out -out outlier.txt
