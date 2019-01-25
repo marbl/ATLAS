f3979877f324d38d411d9b65bb2e36fd42d38f49
shahnidhi <nidhirshah1992@gmail.com>
2019-01-25 11:29:37 -0500

    Removes low-weight connections in the partitions

--- src/make_partitions.py
+++ src/make_partitions.py
@@ -2,6 +2,7 @@
 import networkx as nx
 import community
 import argparse
+import numpy as np
 
 def process_graph(edge_list_file, threshold, output_file):
     G = nx.Graph()
@@ -22,13 +23,22 @@ def process_graph(edge_list_file, threshold, output_file):
     subgraphs = list(nx.connected_component_subgraphs(G))
     for subgraph in subgraphs:
         part = community.best_partition(subgraph)
+
+        #check for low-weight connections
+        current_comm_nodes = list(part.keys()) 
+        current_comm = G.subgraph(current_comm_nodes)
+        degree_val = [x[1] for x in current_comm.degree(current_comm_nodes)]
+        deg_med = np.median(degree_val)
+        deg_std = np.std(degree_val)
         for db in part:
+            if current_comm.degree(db) < (deg_med - 2 * deg_std) or current_comm.degree(db) < 2:
+                print (degree_val, deg_med, deg_std, "partition just before-" + str(part[db] + counter))
+                continue
             partition_num = part[db] + counter
             if partition_num > current_highest:
                 current_highest = partition_num
             fw.write(db + '\t' + str(partition_num) + '\n')
         counter = current_highest + 1
-
 
 def main():
 
